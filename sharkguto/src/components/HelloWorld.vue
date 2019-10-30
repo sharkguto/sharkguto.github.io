49c844161afe855e451be26ff0a09bc466b618bc
Gustavo Freitas <gustavo.freitas@brasil317.com.br>
2019-10-30 10:49:21 -0300

    test

--- sharkguto/src/components/HelloWorld.vue
+++ sharkguto/src/components/HelloWorld.vue
@@ -7,16 +7,6 @@
   </div>
 </template>
 
-
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String,
-  },
-};
-</script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
@@ -34,3 +24,13 @@ a {
   color: #42b983;
 }
 </style>
+
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+};
+</script>
