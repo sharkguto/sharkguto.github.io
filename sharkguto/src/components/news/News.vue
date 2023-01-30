614fb868e86263b96d1499098081d5f355a43e26
Gustavo Freitas <gustavo.freitas@brasil317.com.br>
2023-01-30 15:51:41 -0300

    ajustes

--- sharkguto/src/components/news/News.vue
+++ sharkguto/src/components/news/News.vue
@@ -1,15 +1,19 @@
 <script setup>
-import { ref,reactive, onUpdated } from 'vue'
+import { ref, onUpdated , onMounted} from 'vue'
 import {useRoute} from 'vue-router'
 
 let route = useRoute();
 console.log(route);
 let account = ref(route.params.account);
 
-
+onMounted(() => {
+    console.log("onMounted news");
+    account.value = route.params.account;
+})
 
 onUpdated(() => {
-    account = route.params.account;
+    console.log("onUpdated news");
+    account.value = route.params.account;
 })
 
 </script>
