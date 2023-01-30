66914a89a7ab39a777d6e0b9826eaed19f3f6293
Gustavo Freitas <gustavo.freitas@brasil317.com.br>
2023-01-30 11:39:55 -0300

    fix logo gmf-tech ok

--- sharkguto/src/App.vue
+++ sharkguto/src/App.vue
@@ -5,11 +5,9 @@ import HelloWorld from './components/HelloWorld.vue'
 <template>
   <div>
     <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
+      <img src="/favicon.svg" class="logo" alt="Vite logo" />
     </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+
   </div>
   <HelloWorld msg="Vite + Vue" />
 </template>
@@ -20,9 +18,11 @@ import HelloWorld from './components/HelloWorld.vue'
   padding: 1.5em;
   will-change: filter;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
