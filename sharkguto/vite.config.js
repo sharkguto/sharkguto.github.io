83941cc8129c9070d2116ec27b69cd433d768d2f
Gustavo Freitas <gustavo.freitas@brasil317.com.br>
2023-01-30 11:12:09 -0300

    init project

---
+++ sharkguto/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { fileURLToPath } from "url";
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+    server: {
+        port: 3000,
+    },
+    build: {
+        brotliSize: false, // unsupported in StackBlitz
+    },
+});
